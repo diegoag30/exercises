c48ea27412e362d5efb7e5c7c7da8f2557ab27bd
diego <diego.pablo.aguilar@gmail.com>
2018-03-22 12:09:46 -0300

    Excercise 4 fixed

--- excercises.js
+++ excercises.js
@@ -5,25 +5,8 @@
 var toPrint;
 var x = 1;
 
-while (x<21); {
-    if(x%3===0){
-        if(x%5===0){
-            toPrint==="JuliaJames";
-            x=x+1;
-        }
-        else{
-            toPrint==="Julia";
-            x=x+1;
-        }
-    }
-    else if(x%5===0){
-        toPrint==="James";
-        x=x+1;
-    }
-    else{
-        toPrint=== x;
-        x=x+1;
-    }// check divisibility
+while (x<21) {
+    toPrint = (x%3===0) ? ((x%5===0) ? "JuliaJames": "Julia"):((x%5===0) ? "James" : x ); 
     console.log(toPrint);// print Julia, James, or JuliaJames
     x = x+1;// increment x 
 }
